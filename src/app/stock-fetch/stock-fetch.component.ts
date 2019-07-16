05e67881ac547ea046083b1ec73dbbc1d26814b2
Jerome Flaherty <jayromulus.f@gmail.com>
2019-07-16 11:37:18 -0400

    things are working

--- src/app/stock-fetch/stock-fetch.component.ts
+++ src/app/stock-fetch/stock-fetch.component.ts
@@ -22,15 +22,15 @@ export class StockFetchComponent implements OnInit {
     private dbService: DatabaseService,
   ) { }
 
-  buttonThing() {
-    this.names = this.dbService.getNames()
+  buttonThing(input) {
     // console.log('names: ', this.names)
-    this.url = this.baseurl + this.names + '&tsyms=USD'
-    this.http.get(this.url).subscribe((dat: any) => {this.display = dat})
-    console.log(this.display)
+    this.url = this.baseurl + input + '&tsyms=USD'
+    this.http.get(this.url).subscribe((dat: any) => {this.display = dat; console.log(this.display)})
+    
   }
 
   ngOnInit() {
+    this.names = this.dbService.getNames()
     // this.dbService.coinlist().subscribe((data: any) => { console.log(data) })
     // this.url = this.baseurl + this.names + '&tsyms=USD'
     // this.http.get(this.url).subscribe((dat: any) => {console.log(dat)})
