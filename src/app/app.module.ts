47ed3684bd9b61686c9799d9b0e2b7fc0bd86279
Jerome Flaherty <jayromulus.f@gmail.com>
2019-07-17 16:00:35 -0400

    lsast push on wednesday

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,37 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { SingupLoginComponent } from './singup-login/singup-login.component'
 import { MatGridListModule} from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { StockFetchComponent } from './stock-fetch/stock-fetch.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ReactiveFormsModule } from '@angular/forms'
 
 
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SingupLoginComponent,
-    StockFetchComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatCardModule,
-    MatGridListModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    MatButtonToggleModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent,
+   StockFetchComponent,
+   NavbarComponent,
+ ],
+ imports: [
+   BrowserModule,
+   AppRoutingModule,
+   MatToolbarModule,
+   MatButtonModule,
+   MatCardModule,
+   MatGridListModule,
+   BrowserAnimationsModule,
+   HttpClientModule,
+   MatButtonToggleModule,
+   ReactiveFormsModule
+ ],
+ providers: [],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
