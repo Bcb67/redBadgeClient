ded8fdefbad35dd198169b655939aa6f0e4910ff
Jerome Flaherty <jayromulus.f@gmail.com>
2019-07-22 09:54:57 -0400

    for cody

--- src/app/stock-fetch/stock-fetch.component.ts
+++ src/app/stock-fetch/stock-fetch.component.ts
@@ -9,20 +9,25 @@ import { NgxSpinnerService } from "ngx-spinner";
   styleUrls: ['./stock-fetch.component.css'],
 })
 export class StockFetchComponent implements OnInit {
-
   names = [];
   display: any;
   display2: any;
   prices = [];
   click = false;
   coins = [];
+  disp = false;
+  top24: any;
+
   constructor(
     private dbService: DatabaseService,
     private spinner: NgxSpinnerService
   ) { }
 
-  buttonThing() {
+  tableButtonThing() {
     this.display = this.dbService.fetchSymbolInfo(this.names)
+    this.display2 = []
+    this.prices = []
+    this.coins = []
     this.display2 = this.dbService.getPortfolioValues(this.display)
     this.prices = this.dbService.symPrice
     this.coins = this.dbService.getCoinNames()
@@ -30,27 +35,62 @@ export class StockFetchComponent implements OnInit {
     // this.dbService.getdbObject()
     return this.display2
   }
-
+  
   ngOnInit() {
+    // Table onIit Stuff
     /** spinner starts on init */
-    this.spinner.show();
-    setTimeout(() => {
-      /** spinner ends after 5 seconds */
-      this.spinner.hide();
-    }, 3000);
-    this.names = this.dbService.getSymbolNames()
-    this.display = this.dbService.fetchSymbolInfo(this.names)
-    this.display2 = this.dbService.getPortfolioValues(this.display)
-    this.coins = this.dbService.getCoinNames()
+    // this.spin()
+    // this.names = this.dbService.getSymbolNames()
+    // this.display = this.dbService.fetchSymbolInfo(this.names)
+    // this.display2 = this.dbService.getPortfolioValues(this.display)
+    // this.coins = this.dbService.getCoinNames()
 
-    
+    // cards
+    // this.top24 = this.dbService.getTop24hr()
+    this.spin()
+  }
+
+  button() {
+    this.spin()
+    console.log(this.top24);
+  }
+
+  spin() {
+    if(this.dbService.firstTime){
+      this.spinner.show();
+      setTimeout(() => {
+        /** spinner ends after 5 seconds */
+        // this.spinner.hide();
+        this.top24 = this.dbService.getTop24hr()
+        setTimeout(() => {
+          /** spinner ends after 5 seconds */
+          // this.spinner.hide();
+          this.disp = true;
+        }, 1500);
+      }, 3000);
+    }
+    this.dbService.firstTime = false;
   }
 
   ngAfterViewInit() {
     setTimeout(() => {
-      this.buttonThing()
+      this.tableButtonThing()
     }, 1000)
     // 500 for local fetch, 1000 for tablefetch
   }
-
 }
+
+
+// [
+//   {
+//     CoinInfo: {
+//       FullName: 'BitCoin',
+//       Name: 'BTC',
+//       ImageUrl: '/media/19633/btc.png'
+//     },
+//     DISPLAY: {
+//       CHANGEPCT24HOUR: '-2.24',
+//       PRICE: '$223345'
+//     }
+//   }
+// ]
