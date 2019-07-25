c9b51548414397d3f6f142247b4e5e31735f8fce
unknown <oldrustymailbox@gmail.com>
2019-07-24 22:55:18 -0400

    its getting late

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignuppComponent } from './signupp/signupp.component';
 import { AuthGuardService as AuthGuard } from './auth-guard.service'
-// import { PortfolioComponent } from './portfolio/portfolio.component'
+import { PortfolioComponent } from './portfolio/portfolio.component'
 import { LandingComponent } from './landing/landing.component';
 export const routes: Routes = [
   {path: "login", component: LoginComponent},
   {path: "signup", component: SignuppComponent},
   {path: "", component: LandingComponent},
-  // {path: "portfolio", component: PortfolioComponent, canActivate: [AuthGuard]}
+  {path: "portfolio", component: PortfolioComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
